a6c9c3bea26ae2e0c42fd46ca2f99d2cd644eac7
unknown <24114290093@academico.iesb.br>
2026-03-27 08:30:55 -0300

    feat: projeto inicial

---
+++ GIT1/script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').addEventListener('click', function () {
+  window.open(
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1',
+    '_blank'
+  );
+});
